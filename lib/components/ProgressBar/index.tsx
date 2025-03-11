7f2fcf64fa38f5766d65c87822aa7a9231dda349
Lisuuuuu <Antek.Lisowski@o2.pl>
2025-03-11 14:52:54 +0100

    rakt 2

--- lib/components/ProgressBar/index.tsx
+++ lib/components/ProgressBar/index.tsx
@@ -1,6 +1,8 @@
 'use client';
+
 import { ProgressBarStyles } from './styles';
 import { useState, useEffect } from 'react';
+
 
 export default function ProgressBar() {
 
@@ -8,24 +10,34 @@ export default function ProgressBar() {
 
 
     const classes = ProgressBarStyles();
-    const [position, setPosition] = useState(1584);
+    const [position, setPosition] = useState(0);
+    const handleClick = (event: React.MouseEvent) => {
+
+        setPosition(event.clientX - 1350);
+    };
 
     return (
-        <div onClick={(position) =>
-            setPosition(position.clientX)
-        }>
+    <div className={classes.group}>
+    <div onClick={handleClick}>
 
-        <div className={classes.progress}>
-            
-        </div>
+        <div className={classes.progress}
+        style = {{
+            '--progress-width': (position) + 'px',
+        } as React.CSSProperties}>
+
+                
+
         <div className={classes.dot}
         style={{
-            left: position,
-            
+             left: position,
+                
         }}>
 
         </div>
         </div>
+    </div>
+    </div>
+
 
     )
 }
--- lib/components/ProgressBar/styles.ts
+++ lib/components/ProgressBar/styles.ts
@@ -1,10 +1,16 @@
 import { sva } from "@/styled-system/css";
 
 
+
 export const ProgressBarStyles = sva({
-    slots: ['progress', 'dot'],
+    slots: ['progress', 'dot', 'group'],
+
+
+    //backgroundColor: '#00ff3c'
+
 
     base: {
+
         progress: {
             width: '500px',
             height: '5px',
@@ -15,28 +21,41 @@ export const ProgressBarStyles = sva({
             alignItems: 'center',
             position: 'relative',
 
-            '&:hover': {
-                backgroundColor: '#00ff3c',
-                '& .dot': {
+            _before: {
+                content: "''",
+                height: '4px',
+                backgroundColor: 'white',
+                display: 'block',
+                borderRadius: '5px',
+                position: 'absolute',
+                left: '0',
+                top: '0',
+                width: 'var(--progress-width)',
 
-                },
+            },
+
+            '&:hover [data-dot]': {
+                display: 'block',
             }
+
         },
+
         dot: {
             height: '14px',
             width: '14px',
             borderRadius: '10px',
             backgroundColor: 'white',
-            top: '40px',
+
             position: 'absolute',
+            display: 'none',
 
 
-            '&:hover': {
-                cursor: 'pointer',
-            },
 
 
-        }
+
+
+        },
+
 
     }
 })
